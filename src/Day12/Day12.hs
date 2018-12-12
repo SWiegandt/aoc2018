b8924224cf20f1204dab5f8345247b7990f22fbf
Sebastian Wiegandt <sebastian.wiegandt@gmail.com>
2018-12-12 17:46:25 +0100

    Cheaty day 12 solution

--- src/Day12/Day12.hs
+++ src/Day12/Day12.hs
@@ -1,14 +1,53 @@
 module Day12.Day12 where
 
 import           Util.IO
-import           Day12.Problem1                as P1
-import           Day12.Problem2                as P2
+import           Control.Monad.Trans.State
+import           Data.List
+import           Control.Arrow
+import           Safe.Exact
+import           Data.Maybe
+import qualified Data.Map.Strict               as M
+
+type Pot = Char
+type Rules = M.Map [Pot] Bool
+
+parseInput :: IO ([Pot], Rules)
+parseInput = do
+    input <- filter (not . null) . lines <$> getInput 12
+    return
+        (last . words . head $ input, M.fromList $ map parseRule (tail input))
+
+parseRule :: String -> ([Pot], Bool)
+parseRule s = let [r, _, b : _] = words s in (r, b == '#')
+
+expandRow :: State ([Pot], Int) ()
+expandRow = modify $ \(ps, n) -> ("....." ++ ps ++ ".....", n - 5)
+
+applyRules :: Rules -> State ([Pot], Int) ()
+applyRules rules = modify $ \(pots, n) ->
+    let groups = mapMaybe (takeExactMay 5) . tails $ pots
+    in  (map (\g -> if rules M.! g then '#' else '.') $! groups, n + 2)
+
+dropEmpty :: State ([Pot], Int) ()
+dropEmpty = do
+    (empty, rest) <-
+        second (reverse . dropWhile (== '.') . reverse)
+        .   span (== '.')
+        .   fst
+        <$> get
+    modify $ \(_, n) -> (rest, n + length empty)
+
+runGame :: Rules -> Int -> State ([Pot], Int) Int
+runGame rules 0 =
+    sum
+        .   map snd
+        .   filter ((== '#') . fst)
+        .   (\(pots, n) -> zip pots (map (+ n) [0 ..]))
+        <$> get
+runGame rules n =
+    expandRow >> applyRules rules >> dropEmpty >> runGame rules (n - 1)
 
 main :: IO ()
 main = do
-    problem <- read <$> prompt "Enter problem: " :: IO Int
-
-    case problem of
-        1 -> P1.main
-        2 -> P2.main
-        _ -> error "Problem not yet implemented!"
+    (ns, rules) <- parseInput
+    printWithTime $ evalState (runGame rules 50000) (ns, 0)
--- src/Day12/Problem2.hs
+++
@@ -1,7 +0,0 @@
-module Day12.Problem2 where
-
-import           Util.IO
-import           Day12.Problem1                as P1
-
-main :: IO ()
-main = undefined
