027e69d8776ec6ad16efdfad6ec65f3986263a0c
Sebastian Wiegandt <sebastian.wiegandt@gmail.com>
2018-12-14 19:34:13 +0100

    Solve day 14

--- src/Day14/Day14.hs
+++ src/Day14/Day14.hs
@@ -2,32 +2,50 @@ module Day14.Day14 where
 
 import           Util.IO
 import           Data.Char
-import           Debug.Trace
-import qualified Data.Map.Strict               as M
+import qualified Data.Sequence                 as S
+import           Data.Maybe
+import           Data.List
 
-createRecipe :: (M.Map Int Int, [Int]) -> (M.Map Int Int, [Int])
+type Elves = [Int]
+
+createRecipe :: (S.Seq Int, Elves) -> (S.Seq Int, Elves)
 createRecipe (recipes, elves) =
-    let elfRecipes     = map (recipes M.!) elves
+    let elfRecipes     = mapMaybe (recipes S.!?) elves
         createdRecipes = map digitToInt . show . sum $ elfRecipes
-        recipes'       = foldr (\(n, r) -> M.insert n r) recipes
-            $ zip [M.size recipes ..] createdRecipes
+        recipes'       = foldl' (S.|>) recipes createdRecipes
     in  ( recipes'
-        , zipWith (\e r -> (e + r + 1) `mod` M.size recipes') elves elfRecipes
+        , zipWith (\e r -> (e + r + 1) `mod` S.length recipes') elves elfRecipes
         )
+
+extractFollowing :: Int -> [(S.Seq Int, Elves)] -> String
+extractFollowing input =
+    concatMap show . S.take 10 . S.drop input . fst . head . dropWhile
+        (\(seq, _) -> S.length seq < (input + 10))
+
+firstInstance :: Int -> [(S.Seq Int, Elves)] -> Int
+firstInstance input =
+    let input' = map digitToInt . reverse . show $ input
+    in  subtract (length input')
+        . S.length
+        . (\s@(s' S.:|> _) -> if s' `endsWith` input' then s' else s)
+        . head
+        . dropWhile
+              (\s@(s' S.:|> _) -> not . any (`endsWith` input') $ [s, s'])
+        . map fst
+
+endsWith :: S.Seq Int -> [Int] -> Bool
+endsWith (s' S.:|> n') (n : ns) = n == n' && endsWith s' ns
+endsWith _             []       = True
+endsWith _             _        = False
 
 main :: IO ()
 main = do
-    let input = 440231
+    let input   = 440231
+    let init    = (S.fromList [3, 7], [0 .. 1])
+    let recipes = iterate createRecipe init
 
     -- part 1
-    printWithTime
-        . concatMap show
-        . take 10
-        . drop input
-        . map snd
-        . M.toList
-        . fst
-        . head
-        . dropWhile ((< (input + 10)) . M.size . fst)
-        . iterate createRecipe
-        $ (M.fromList $ [0 ..] `zip` [3, 7], [0 .. 1])
+    printWithTime . extractFollowing input $ recipes
+
+    -- part 2
+    printWithTime . firstInstance input $ recipes
